d04e12ee3e1f1714722bc82b292f2fbf2f0afbd3
Stefan Bruck <stefan.bruck@agilent.com>
2015-10-26 16:55:09 +0100

    prepared for displaying ingredients

--- WebApp/instrumentsController.js
+++ WebApp/instrumentsController.js
@@ -2,8 +2,9 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
     $scope.newInstrumentName = "";
     $scope.newQRCode = 'https://chart.googleapis.com/chart?cht=qr&chl=kitchen%20balance&chs=180x180&choe=UTF-8&chld=L|2';
     $scope.instruments = [];
-    $scope.ingredients = [{name:"flour",unit:"g",amount:"300"}];
-    $scope.serverURL = "http://10.52.4.100:8080"
+    //$scope.ingredients = [];
+    $scope.serverURL = "http://10.52.4.119:8080"
+    //$scope.serverURL = "http://10.52.4.100:8080"
     
     $scope.registerInstrument = function() {
         var config = {
@@ -16,7 +17,6 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
 					.success(
 						function(data, status, headers, config) {
                             $scope.instruments.push({name:$scope.newInstrumentName,qrcode:data.qrCode});  
-                            console.log(data.qrCode);
                     	})
 					.error(
 						function(data, status, headers, config) {
@@ -30,5 +30,23 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
                         function() {
                             $scope.newInstrumentName = "";
                         });
-    }
+    };
+    
+    $scope.refreshIngredients = function() {
+        var config = {
+					headers : {
+						'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+					}
+				};
+        
+            console.log($scope.serverURL + '/ingredient/list');
+            $http.get($scope.serverURL + '/ingredient/list', config)
+            .then(
+                function(response) {
+                        alert("hello" + response);
+                    },    
+                function(response) {
+								alert("Error :" + response);
+						});
+    };
 });
