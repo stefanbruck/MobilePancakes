b65adb4e3df01a9b5563bd45dff75ca4b6282298
Stefan Bruck <stefan.bruck@agilent.com>
2015-10-26 14:41:48 +0100

    added ingredients

--- WebApp/instrumentsController.js
+++ WebApp/instrumentsController.js
@@ -2,6 +2,7 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
     $scope.newInstrumentName = "";
     $scope.newQRCode = 'https://chart.googleapis.com/chart?cht=qr&chl=kitchen%20balance&chs=180x180&choe=UTF-8&chld=L|2';
     $scope.instruments = [];
+    $scope.ingredients = [{name:"flour",unit:"g",amount:"300"}];
     $scope.registerInstrument = function() {
             $scope.instruments.push({name:$scope.newInstrumentName,qrcode:$scope.newQRCode});    
             $scope.newInstrumentName = "";
