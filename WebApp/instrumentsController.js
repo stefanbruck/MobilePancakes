f14baaeeec19b791d89d60f9a45ba1a228e11274
Stefan Bruck <stefan.bruck@agilent.com>
2015-10-26 14:14:21 +0100

    separated view from controller

---
+++ WebApp/instrumentsController.js
@@ -0,0 +1,9 @@
+angular.module('webApp', []).controller('instrumentsController', function($scope) {
+    $scope.newInstrumentName = "";
+    $scope.newQRCode = 'https://chart.googleapis.com/chart?cht=qr&chl=kitchen%20balance&chs=180x180&choe=UTF-8&chld=L|2';
+    $scope.instruments = [];
+    $scope.registerInstrument = function() {
+            $scope.instruments.push({name:$scope.newInstrumentName,qrcode:$scope.newQRCode});    
+            $scope.newInstrumentName = "";
+    }
+});
