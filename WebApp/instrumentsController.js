6945e4ef814c273871e09836673b7759dec3cb62
Stefan Bruck <stefan.bruck@agilent.com>
2015-10-27 09:24:06 +0100

    Added refresh

--- WebApp/instrumentsController.js
+++ WebApp/instrumentsController.js
@@ -5,48 +5,55 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
     //$scope.ingredients = [];
     $scope.serverURL = "http://10.52.4.119:8080"
     //$scope.serverURL = "http://10.52.4.100:8080"
+    var config = {
+					headers : {
+						'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+					}
+				};
     
     $scope.registerInstrument = function() {
-        var config = {
-					headers : {
-						'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-					}
-				};
-        
-            $http.post($scope.serverURL + '/instrument/register', 'name=' + $scope.newInstrumentName, config)
-					.success(
-						function(data, status, headers, config) {
-                            $scope.instruments.push({name:$scope.newInstrumentName,qrcode:data.qrCode});  
-                    	})
-					.error(
-						function(data, status, headers, config) {
-							if (status == 400) {
-								alert(data.error_description);
-							} else {
-								alert("Error :" + status);
-							}
-						})
-                    .then(
-                        function() {
-                            $scope.newInstrumentName = "";
-                        });
+        $http.post($scope.serverURL + '/instrument/register', 'name=' + $scope.newInstrumentName, config)
+                .then(
+                    function(response) {
+                        $scope.refreshInstruments();
+                    },
+                    function(response) {
+                        alert("Error registering instrument");
+                    })
+                .finally(
+                    function() {
+                        $scope.newInstrumentName = "";
+                    });
     };
     
-    $scope.refreshIngredients = function() {
-        var config = {
-					headers : {
-						'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-					}
-				};
+    $scope.onInstrumentNameKeyPress = function(event)
+    {
+        if (event.charCode == 13) //if enter then hit the search button
+            $scope.registerInstrument();
+    }
+    
+    $scope.refreshInstruments = function() {
+        $http.get($scope.serverURL + '/instrument/list', config)
+                .then(
+                    function(response) {
+                        $scope.instruments = response.data;
+                    },    
+                    function(response) {
+                        alert("Error :" + response);
+                });
         
-            console.log($scope.serverURL + '/ingredient/list');
-            $http.get($scope.serverURL + '/ingredient/list', config)
-            .then(
-                function(response) {
+    }
+    
+    $scope.refreshIngredients = function() {    
+        $http.get($scope.serverURL + '/ingredient/list', config)
+                .then(
+                    function(response) {
                         alert("hello" + response);
                     },    
-                function(response) {
-								alert("Error :" + response);
-						});
+                    function(response) {
+                        alert("Error :" + response);
+                });
     };
+    
+    $scope.refreshInstruments();
 });
