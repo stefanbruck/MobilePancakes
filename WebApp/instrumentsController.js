9fad7d4c8fdad717781e0add576c62ec7d7d6b5a
Stefan Bruck <stefan.bruck@agilent.com>
2015-10-27 10:14:29 +0100

    Open full QRCode on click#

--- WebApp/instrumentsController.js
+++ WebApp/instrumentsController.js
@@ -5,6 +5,8 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
     //$scope.ingredients = [];
     $scope.serverURL = "http://10.52.4.119:8080"
     //$scope.serverURL = "http://10.52.4.100:8080"
+    $scope.selectedQRCode;
+    
     var config = {
 					headers : {
 						'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
@@ -54,6 +56,12 @@ angular.module('webApp', []).controller('instrumentsController', function($scope
                         alert("Error :" + response);
                 });
     };
+    
+    $scope.showQRCode = function(qrCode) {
+        var props = 'resizable= yes; status= no; scroll= no; help= no; center= yes;width=100;height=100;menubar=no;directories=no;location=no;modal=yes'
+        var w = window.open("", "QRCode of Instrument", props);
+        w.document.write("<img src='" + qrCode +"'></img>")
+    }
     
     $scope.refreshInstruments();
 });
