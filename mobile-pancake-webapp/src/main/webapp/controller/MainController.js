bdd46d0ab9e26d243b8a0643e53ec7c9a4fd9864
Christian <christian.lueder@non.agilent.com>
2015-10-27 18:09:01 +0100

    Implement sending of the measures

--- mobile-pancake-webapp/src/main/webapp/controller/MainController.js
+++ mobile-pancake-webapp/src/main/webapp/controller/MainController.js
@@ -1,5 +1,28 @@
 angular.module('mobilePancake')
 .controller('MainController', function ($http, $scope) {
+    $scope.value = 123;
+    $scope.unit = 'g';
+    
+    $scope.send = function(newValue) {
+        var payload = 'recipeName=' + $scope.selectedRecipe + '&selectedIngredient=' + $scope.selectedIngredient + '&value='+ $scope.value + '&unit' + $scope.unit;
+        
+        var config = {
+            headers : {
+                'Content-Type': 'x-www-form-urlencoded; charset=UTF-8',
+                'Access-Control-Allow-Origin' : '*'
+            }
+        };
+        
+      $http.post('http://10.52.4.100:8080/measures/save', payload, config)
+        .success(function(data, status, headers, config) {
+          alert('Saved');
+        })
+        .error(function(data, status, headers, config) {
+          alert('error');
+        });
+    };
+    
+
     $http.get('http://10.52.4.100:8080/recipe/list')
         .success(
           function(data, status, headers, config) {
